22b228235725c98690629a23923c3480735b57ab
Sai Eswar Boda <bodasaieswar@gmail.com>
2022-11-28 01:53:17 +0000

    First version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '75a680cf7c23ca47e917aa522787f41945cf1708',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '75a680cf7c23ca47e917aa522787f41945cf1708',
+    ),
+  ),
+);
